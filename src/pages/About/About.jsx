93825d377185bb360b580fb6d7af807699c9e4b2
Modestas Skirius <modestas.skirius@gmail.com>
2021-01-11 18:46:19 +0200

    add about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./About.scss";
+import me from "./../../assets/me.jpg";
 
 function About() {
-  return <div>About page</div>;
+  return (
+    <div className="about">
+      <div className="about__main">
+        <div className="about__main--text-box">
+          <h1>About</h1>
+          <article>
+            With passion recently graduated Frond-end course at Code Academy in
+            Vilnius. I learned many things, such as front-end development with
+            HTML, CSS and JavaScript, various popular libraries and frameworks
+            (Vue.js, React, Bulma, Material-ui), writing clean code, debugging,
+            working in a team, using Git/GitHub to manage and contribute code to
+            a team project, and most importantly - I learned how to learn new
+            things.
+          </article>
+        </div>
+        <div className="about__main--image-box">
+          <div></div>
+          {/* <img src={me} alt="me" /> */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
