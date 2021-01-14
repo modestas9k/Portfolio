f4ddbeff71965309891deb45ef901d568af62c9c
Modestas Skirius <modestas.skirius@gmail.com>
2021-01-14 19:13:32 +0200

    changed directory

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Header } from "./Header/Header";
 export { default as Button } from "./Button/Button";
 export { default as Card } from "./Card/Card";
-export { default as FlippingCard } from "./FlippingCard/FlippingCard";
