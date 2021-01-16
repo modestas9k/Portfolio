47c8db102a5ae5b112ebd083d9de80d7a4601845
Modestas Skirius <modestas.skirius@gmail.com>
2021-01-16 17:42:43 +0200

    drumKit final

--- src/DailyChallanges/DrumKit/DrumKit.jsx
+++ src/DailyChallanges/DrumKit/DrumKit.jsx
@@ -1,18 +1,26 @@
 import React, { useEffect, useState } from "react";
 import "./DrumKit.scss";
 import UIfx from "uifx";
-import clapSound from "./../../assets/DrumKit-samples/909-Clap-1.wav";
-import kickSound from "./../../assets/DrumKit-samples/909-Kick-T0A0A7.wav";
-import hiHatSound from "./../../assets/DrumKit-samples/909-HiHatOpen-D4.wav";
-import openHatSound from "./../../assets/DrumKit-samples/909-HiHatClosed-D4.wav";
-import boomSound from "./../../assets/DrumKit-samples/909-Kick-TAAADA.wav";
-import rideSound from "./../../assets/DrumKit-samples/909-Ride-D0.wav";
-import snareSound from "./../../assets/DrumKit-samples/909-Snare-T0T3SA.wav";
-import lowTomSound from "./../../assets/DrumKit-samples/909-LoTom-0D7.wav";
-import midTomSound from "./../../assets/DrumKit-samples/909-MidTom-0D7.wav";
+import clapSound from "./../../assets/DrumKit-samples/Clap.wav";
+import kickSound from "./../../assets/DrumKit-samples/Kick.wav";
+import crashSound from "./../../assets/DrumKit-samples/Crash.wav";
+import openHatSound from "./../../assets/DrumKit-samples/HiHatClosed.wav";
+import boomSound from "./../../assets/DrumKit-samples/KickBoom.wav";
+import rideSound from "./../../assets/DrumKit-samples/Ride.wav";
+import snareSound from "./../../assets/DrumKit-samples/Snare.wav";
+import lowTomSound from "./../../assets/DrumKit-samples/LoTom.wav";
+import midTomSound from "./../../assets/DrumKit-samples/MidTom.wav";
 
 function DrumKit() {
-  //const [activeKey, setActiveKey] = useState("");
+  const [isClap, setClap] = useState(false);
+  const [isKick, setKick] = useState(false);
+  const [isCrash, setCrash] = useState(false);
+  const [isOpenHat, setOpenHat] = useState(false);
+  const [isBoom, setBoom] = useState(false);
+  const [isRide, setRide] = useState(false);
+  const [isSnare, setSnare] = useState(false);
+  const [isLowTom, setLowTom] = useState(false);
+  const [isMidTom, setMidTom] = useState(false);
 
   const clap = new UIfx(clapSound, {
     volume: 0.4, // number between 0.0 ~ 1.0
@@ -22,7 +30,7 @@ function DrumKit() {
     volume: 0.4,
     throttleMs: 100,
   });
-  const hiHat = new UIfx(hiHatSound, {
+  const crash = new UIfx(crashSound, {
     volume: 0.4,
     throttleMs: 100,
   });
@@ -50,32 +58,76 @@ function DrumKit() {
     volume: 0.4,
     throttleMs: 100,
   });
-
-  window.addEventListener("keydown", (e) => {
-    if (e.code === "KeyA") {
-      return clap.play();
-    } else if (e.code === "KeyS") {
-      return hiHat.play();
-    } else if (e.code === "KeyD") {
-      return kick.play();
-    } else if (e.code === "KeyF") {
-      return openHat.play();
-    } else if (e.code === "KeyG") {
-      return boom.play();
-    } else if (e.code === "KeyH") {
-      return ride.play();
-    } else if (e.code === "KeyJ") {
-      return snare.play();
-    } else if (e.code === "KeyK") {
-      return lowTom.play();
-    } else if (e.code === "KeyL") {
-      return midTom.play();
+  const eventHandler = (key) => {
+    if (key === "KeyA") {
+      clap.play();
+      setClap(true);
+      setTimeout(() => {
+        setClap(false);
+      }, 300);
+    } else if (key === "KeyS") {
+      crash.play();
+      setCrash(true);
+      setTimeout(() => {
+        setCrash(false);
+      }, 300);
+    } else if (key === "KeyD") {
+      kick.play();
+      setKick(true);
+      setTimeout(() => {
+        setKick(false);
+      }, 300);
+    } else if (key === "KeyF") {
+      openHat.play();
+      setOpenHat(true);
+      setTimeout(() => {
+        setOpenHat(false);
+      }, 300);
+    } else if (key === "KeyG") {
+      boom.play();
+      setBoom(true);
+      setTimeout(() => {
+        setBoom(false);
+      }, 300);
+    } else if (key === "KeyH") {
+      ride.play();
+      setRide(true);
+      setTimeout(() => {
+        setRide(false);
+      }, 300);
+    } else if (key === "KeyJ") {
+      snare.play();
+      setSnare(true);
+      setTimeout(() => {
+        setSnare(false);
+      }, 300);
+    } else if (key === "KeyK") {
+      lowTom.play();
+      setLowTom(true);
+      setTimeout(() => {
+        setLowTom(false);
+      }, 300);
+    } else if (key === "KeyL") {
+      midTom.play();
+      setMidTom(true);
+      setTimeout(() => {
+        setMidTom(false);
+      }, 300);
     }
-  });
-  console.log(clap);
-  // useEffect(() => {
-  //   console.log(activeKey);
-  // }, [activeKey]);
+  };
+
+  useEffect(() => {
+    const onKeyDown = ({ code }) => {
+      eventHandler(code);
+    };
+
+    document.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", onKeyDown);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="drum-kit">
@@ -83,41 +135,65 @@ function DrumKit() {
       <h1>DrumKit</h1>
       <div className="drum-kit__keys-wrapper">
         <div
-          data-key="65"
-          className={`drum-kit__key ${onkeydown && "playing"}`}
+          className={`drum-kit__key ${isClap ? "playing" : ""} `}
+          onClick={() => eventHandler("KeyA")}
         >
           <kbd>A</kbd>
           <span className="drum-kit__sound">Clap</span>
         </div>
-        <div data-key="83" className="drum-kit__key">
+        <div
+          className={`drum-kit__key ${isCrash ? "playing" : ""} `}
+          onClick={() => eventHandler("KeyS")}
+        >
           <kbd>S</kbd>
-          <span className="drum-kit__sound">hihat</span>
+          <span className="drum-kit__sound">crash</span>
         </div>
-        <div data-key="68" className="drum-kit__key">
+        <div
+          className={`drum-kit__key ${isKick ? "playing" : ""} `}
+          onClick={() => eventHandler("KeyD")}
+        >
           <kbd>D</kbd>
           <span className="drum-kit__sound">kick</span>
         </div>
-        <div data-key="70" className="drum-kit__key">
+        <div
+          className={`drum-kit__key ${isOpenHat ? "playing" : ""} `}
+          onClick={() => eventHandler("KeyF")}
+        >
           <kbd>F</kbd>
-          <span className="drum-kit__sound">openhat</span>
+          <span className="drum-kit__sound">close hat</span>
         </div>
-        <div data-key="71" className="drum-kit__key">
+        <div
+          className={`drum-kit__key ${isBoom ? "playing" : ""} `}
+          onClick={() => eventHandler("KeyG")}
+        >
           <kbd>G</kbd>
           <span className="drum-kit__sound">boom</span>
         </div>
-        <div data-key="72" className="drum-kit__key">
+        <div
+          className={`drum-kit__key ${isRide ? "playing" : ""} `}
+          onClick={() => eventHandler("KeyH")}
+        >
           <kbd>H</kbd>
           <span className="drum-kit__sound">ride</span>
         </div>
-        <div data-key="74" className="drum-kit__key">
+        <div
+          className={`drum-kit__key ${isSnare ? "playing" : ""} `}
+          onClick={() => eventHandler("KeyJ")}
+        >
           <kbd>J</kbd>
           <span className="drum-kit__sound">snare</span>
         </div>
-        <div data-key="75" className="drum-kit__key">
+        <div
+          className={`drum-kit__key ${isLowTom ? "playing" : ""} `}
+          onClick={() => eventHandler("KeyK")}
+        >
           <kbd>K</kbd>
           <span className="drum-kit__sound">Low tom</span>
         </div>
-        <div data-key="76" className="drum-kit__key">
+        <div
+          className={`drum-kit__key ${isMidTom ? "playing" : ""} `}
+          onClick={() => eventHandler("KeyL")}
+        >
           <kbd>L</kbd>
           <span className="drum-kit__sound">mid tom</span>
         </div>
