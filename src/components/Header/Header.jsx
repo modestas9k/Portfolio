3b35c2e7337022392bb3ce3bcc479a80641fc562
Modestas Skirius <modestas.skirius@gmail.com>
2021-01-17 17:55:47 +0200

    change header to side nav with burger button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.scss";
-import logo from "./../../assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Header() {
-  return (
-    <div className="header__container">
-      <nav>
-        <Link to="/" className="nav__logo">
-          <img src={logo} alt="logo" />
-        </Link>
+  const [isOpen, setOpen] = useState(false);
 
-        <div className="nav__links">
-          <Link to="/projects">Projects</Link>
-          <Link to="/about">About</Link>
+  return (
+    <div>
+      <button
+        onClick={() => setOpen(!isOpen)}
+        className={isOpen ? "burger burger__open" : "burger"}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
+      <nav className={isOpen ? "open" : ""}>
+        <Link onClick={() => setOpen(false)} to="/projects">
+          Projects
+        </Link>
+        <Link onClick={() => setOpen(false)} to="/about">
+          About
+        </Link>
+        <div className="header__line"></div>
+        <div className="header__buttons">
+          <button
+            className="header__btn-linkedin"
+            onClick={() =>
+              window.open(
+                "https://www.linkedin.com/in/modestas-skirius-276631202/"
+              )
+            }
+          >
+            <FontAwesomeIcon icon={["fab", "linkedin-in"]} />
+          </button>
+          <button
+            className="header__btn-github"
+            onClick={() => window.open("https://github.com/modestas9k")}
+          >
+            <FontAwesomeIcon icon={["fab", "github"]} />
+          </button>
+          <button
+            className="header__btn-instagram"
+            onClick={() =>
+              window.open("https://www.instagram.com/modest.skirius/")
+            }
+          >
+            <FontAwesomeIcon icon={["fab", "instagram"]} />
+          </button>
+          <button
+            className="header__btn-mail"
+            onClick={() => window.open("mailto:modestas.skirius@gmail.com")}
+          >
+            <FontAwesomeIcon icon={["far", "envelope"]} />
+          </button>
         </div>
       </nav>
     </div>
