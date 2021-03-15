08a9a5008536e5ad333de644d062f2ba0dffc78a
Modestas Skirius <modestas.skirius@gmail.com>
2021-03-15 13:06:51 +0200

    add projectProvider

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import Routes from "./Routes";
 import "normalize.css";
 import "./fontAwesome.js";
 import "./../src/scss/index.scss";
+import { ProjectProvider } from "./contexts/ProjectContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <ProjectProvider>
+      <Routes />
+    </ProjectProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
